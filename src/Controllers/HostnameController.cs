c37a6c3e8f5cb2e1ca7c4ff732bb1d3df8ea2290
John <jmuller@gsx.com>
2017-02-04 17:29:30 +0100

    Hostname Rest API

---
+++ src/Controllers/HostnameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace hostname_core.Controllers
+{
+    [Route("api/[controller]")]
+    public class HostnameController : Controller
+    {
+        // GET api/hostname
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "hostname" };
+        }
+    }
+}
